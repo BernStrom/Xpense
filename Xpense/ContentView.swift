6a84921ddfaf97688c1ec46d9a88cd96a06779f4
Bern <bernnystrom@gmail.com>
2022-06-20 21:54:27 +0800

    🎉 (new): Initial commit of project base code
    First commit of the app with a starting base layout and functionality.

---
+++ Xpense/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  Xpense
+//
+//  Created by Bern N on 6/20/22.
+//
+
+import SwiftUI
+
+struct ExpenseItem {
+    let name: String
+    let type: String
+    let amount: Double
+}
+
+class Expenses: ObservableObject {
+    @Published var items = [ExpenseItem]()
+}
+
+struct ContentView: View {
+    @StateObject var expenses = Expenses()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(expenses.items, id: \.name) { item in
+                    Text(item.name)
+                }
+            }
+            .navigationTitle("Xpense")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Xpense/XpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  XpenseApp.swift
+//  Xpense
+//
+//  Created by Bern N on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct XpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
