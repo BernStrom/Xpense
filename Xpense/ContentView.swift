0b4739e64a82029cffb3194638c171fa051e78aa
Bern <bernnystrom@gmail.com>
2022-06-24 13:14:37 +0800

    🚸 (new): Separate items type into a personal & business section
    Modified the items list layout into two separate section for personal and business items, with their own methods for removing items from their list section.

--- Xpense/ContentView.swift
+++ Xpense/ContentView.swift
@@ -23,6 +23,14 @@ class Expenses: ObservableObject {
         }
     }
     
+    var personalItems: [ExpenseItem] {
+        items.filter { $0.type == "Personal" }
+    }
+    
+    var businessItems: [ExpenseItem] {
+        items.filter { $0.type == "Business" }
+    }
+    
     init() {
         if let savedItems = UserDefaults.standard.data(forKey: "Items") {
             if let decodedItems = try? JSONDecoder().decode([ExpenseItem].self, from: savedItems) {
@@ -42,21 +50,39 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             List {
-                ForEach(expenses.items) { item in
-                    HStack {
-                        VStack(alignment: .leading) {
-                            Text(item.name)
-                                .font(.headline)
-                            Text(item.type)
+                Section("Personal") {
+                    ForEach(expenses.personalItems) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                .foregroundColor(item.amount >= 100 ? .red : .green)
                         }
-                        
-                        Spacer()
-                        
-                        Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
-                            .foregroundColor(item.amount >= 100 ? .red : .green)
                     }
+                    .onDelete(perform: removePersonalItems)
                 }
-                .onDelete(perform: removeItems)
+                
+                Section("Business") {
+                    ForEach(expenses.businessItems) { item in
+                        HStack {
+                            VStack(alignment: .leading) {
+                                Text(item.name)
+                                    .font(.headline)
+                            }
+                            
+                            Spacer()
+                            
+                            Text(item.amount, format: .currency(code: Locale.current.currencyCode ?? "USD"))
+                                .foregroundColor(item.amount >= 100 ? .red : .green)
+                        }
+                    }
+                    .onDelete(perform: removeBusinessItems)
+                }
             }
             .toolbar {
                 Button {
@@ -72,8 +98,20 @@ struct ContentView: View {
         }
     }
     
-    func removeItems(at offsets: IndexSet) {
-        expenses.items.remove(atOffsets: offsets)
+    func removePersonalItems(at offsets: IndexSet) {
+        for offset in offsets {
+            if let index = expenses.items.firstIndex(where: { $0.id == expenses.personalItems[offset].id }) {
+                expenses.items.remove(at: index)
+            }
+        }
+    }
+    
+    func removeBusinessItems(at offsets: IndexSet) {
+        for offset in offsets {
+            if let index = expenses.items.firstIndex(where: { $0.id == expenses.businessItems[offset].id }) {
+                expenses.items.remove(at: index)
+            }
+        }
     }
 }
 
